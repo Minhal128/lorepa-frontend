b1291859d9abd8ec2ad1cdde0c03ba48340d5f03
Minhal Rizvi <rizvi4600779@cloud.neduet.edu.pk>
2026-02-19 08:39:20 +0500

    completely the admin, web and backend

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: "http://localhost:4000/api/v1",
+  baseUrl: "https://starfish-app-qp7nw.ondigitalocean.app/api/v1",
   GOOGLE_API_KEY: import.meta.env.VITE_GOOGLE_API_KEY || "AIzaSyA37GH1PD5_UId3KXqp_MwkPQOGZv_TOv0",
 };
 
